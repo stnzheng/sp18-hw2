5d1cd0b9e64c2da219780855d1ae4aa01b365c3e
Steven Zheng <stnzheng@gmail.com>
2018-02-19 17:39:44 -0800

    q3

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'pages#home'
   get '/', to: 'pages#home'
-  get '/age', to: 'pages#age'
   post '/', to: 'pages#stringify', as:'stringify'
+
+  get '/age', to: 'pages#age'
   put '/age', to: 'pages#person'
+
+  get '/me', to:'pages#me'
 end
